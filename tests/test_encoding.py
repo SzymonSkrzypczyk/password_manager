3ee728171e1849e3207b04e314ac5f8c9c1d1ffe
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2023-12-20 08:56:22 +0100

    tests for encoding.py

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -3,17 +3,16 @@ import pytest
 
 from src.password_encoding import generate_key, encode_password, decode_password, encode_all, decode_all
 
+# in next commit add fixtures
+TEST_PASSWORD = "TEST"
+key = None
+encoded_password = None
 
-@pytest.fixture()
-def test_password():
+
+def setup_module(_module):
+    global key, encoded_password
     key = generate_key()
-    return encode_password("TEST", key), key
-
-
-@pytest.fixture()
-def encoded_password():
-    key = generate_key()
-    return encode_password("TEST", key), key
+    encoded_password = encode_password(TEST_PASSWORD, key)
 
 
 def test_generate_key_type():
@@ -24,42 +23,44 @@ def test_generate_key_return_not_none():
     assert generate_key() is not None
 
 
-def test_encode_password_type(test_password):
-    # klucz mozna zmienic na fixture
-    print(*test_password)
-    assert type(encode_password(*test_password)) == str
+def test_encode_password_not_none():
+    assert encoded_password is not None
 
 
-'''@pytest.raises(ValueError)
-def test_encode_password_wrong_key():
-    ...
-'''
+def test_encode_password_type():
+    assert type(encoded_password) == str
 
 
-def test_encode_return_not_none(test_password):
-    assert encode_password(*test_password) is not None
+def test_decode_password_not_none():
+    decoded_password = decode_password(encoded_password, key)
+    assert decoded_password is not None
+    assert decoded_password == TEST_PASSWORD
 
 
-def test_decode_password_type(test_password):
-    # fixtures!!!
-    assert type(decode_password(*test_password)) == str
+def test_decode_password_type():
+    decoded_password = decode_password(encoded_password, key)
+    assert type(decoded_password) == str
 
 
-def test_decode_password_return_not_none(test_password):
-    assert decode_password(*test_password) is not None
+def test_encode_all_passwords_not_none():
+    assert encode_all([encoded_password], key) is not None
 
 
-def test_encode_all_return_empty(test_password):
-    assert not encode_all([], test_password[1])
+def test_encode_all_passwords_type():
+    assert type(encode_all([TEST_PASSWORD], key)) == list
 
 
-def test_encode_all_type(test_password):
-    assert type(encode_all(["TEST"], test_password[1])) == list
+def test_decode_all_passwords_not_null():
+    decoded = decode_all([encoded_password], key)
+    assert decoded is not None
+    assert decoded[0] == TEST_PASSWORD
 
 
-def test_decode_all_type(encoded_password):
-    assert type(decode_all([encoded_password[0]], encoded_password[1])) == list
+def test_decode_all_passwords_type():
+    assert type(decode_all([encoded_password], key)) == list
 
 
-def test_decode_all_return_empty(encoded_password):
-    assert not decode_all([], encoded_password[1])
+def teardown_module(_module):
+    global key, encoded_password
+    key = None
+    encoded_password = None
